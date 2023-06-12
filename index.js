3c0657196290201d07f174545777ac1f78042c39
Sandra G <sandyjtech@gmail.com>
2023-06-12 10:01:31 -0700

    added notes to remember the function methos

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+//take a callback function as an argument
+//it doesn't matter what this function returns, so long as it calls the callback function
 function receivesAFunction(callback) {
+  //call the callback function
   callback();
 }
-
+//take no arguments
+//return a named function
 function returnsANamedFunction() {
   return function namedFunction() {
-    // Function logic goes here
+    // Function logic
   };
 }
-
+//take no arguments
+//return an anonymous function
 function returnsAnAnonymousFunction() {
   return function () {
-    // Function logic goes here
+    // Function logic
   };
 }
